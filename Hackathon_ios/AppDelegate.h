fe0664948aa64c16a82f44ffc341841c38d10e59
DungHo <honhantridung@gmail.com>
2016-03-26 12:53:19 +0700

    Initial Commit

---
+++ Hackathon_ios/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Hackathon_ios
+//
+//  Created by KennyHo on 3/26/16.
+//  Copyright © 2016 Mera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Hackathon_ios/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Hackathon_ios
+//
+//  Created by KennyHo on 3/26/16.
+//  Copyright © 2016 Mera. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
